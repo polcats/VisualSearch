74ee84593ae67f7e763d0bd28296a9992c944000
polcats <24451518+polcats@users.noreply.github.com>
2019-12-24 08:29:37 +0800

    Created initial classes.

---
+++ script/development.js
@@ -0,0 +1,13 @@
+class Location {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+class MoveCost {
+    constructor(cost, location) {
+        this.cost = cost;
+        this.location = location;
+    }
+}
