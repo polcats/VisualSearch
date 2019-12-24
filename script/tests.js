35bb2a273f46a52748f5970110fa391268ab1d5c
polcats <24451518+polcats@users.noreply.github.com>
2019-12-24 10:08:27 +0800

    Updated test for invalid locations.

--- script/tests.js
+++ script/tests.js
@@ -6,6 +6,7 @@ function assertEquals(value1, value2, testName) {
 class TestUtility {
     static run() {
         this.test_isValidLocation_invalidLocations();
+        this.test_isNotBlocked_blockedLocations();
     }
 
     static test_isValidLocation_invalidLocations() {
@@ -17,6 +18,26 @@ class TestUtility {
         for (const e in invalidLocations) {
             actual = Utility.isValidLocation(new Location(invalidLocations[0], invalidLocations[1]));
             assertEquals(expected, actual, this.test_isValidLocation_invalidLocations);
+        }
+    }
+
+    static test_isNotBlocked_blockedLocations() {
+        const expected = false;
+        const grid = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ];
+        const blockedLocations = [
+            [0, 0],
+            [1, 1],
+            [2, 2]
+        ];
+
+        let actual = 0;
+        for (let i = 0; i < blockedLocations.length; ++i) {
+            actual = Utility.isNotBlocked(grid, blockedLocations[i][0], blockedLocations[i][1]);
+            assertEquals(expected, actual, this.test_isNotBlocked_blockedLocations);
         }
     }
 }
