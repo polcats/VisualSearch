58bd09b4fa5d85936ca36f50fa425c996e4b839c
polcats <24451518+polcats@users.noreply.github.com>
2019-12-24 10:09:00 +0800

    Updated tests.

--- script/tests.js
+++ script/tests.js
@@ -11,12 +11,12 @@ class TestUtility {
 
     static test_isValidLocation_invalidLocations() {
         const expected = false;
-
-        let invalidLocations = [[-1, 0], [0, -1], [ROW + 1, 0], [COL + 1, 0], [("a", 0)], ["b", 0]];
+        const invalidLocations = [[-1, 0], [0, -1], [ROW + 1, 0], [COL + 1, 0], [("a", 0)], ["b", 0]];
         let actual = 0;
 
-        for (const e in invalidLocations) {
-            actual = Utility.isValidLocation(new Location(invalidLocations[0], invalidLocations[1]));
+        for (let i = 0; i < invalidLocations.length; ++i) {
+            // console.log(invalidLocations[i]);
+            actual = Utility.isValidLocation(new Location(invalidLocations[i][0], invalidLocations[i][1]));
             assertEquals(expected, actual, this.test_isValidLocation_invalidLocations);
         }
     }
