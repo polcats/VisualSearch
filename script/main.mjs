87a2f4f3d269a6bc47ff3a1568645e605515f564
polcats <24451518+polcats@users.noreply.github.com>
2019-12-27 20:24:46 +0800

    Disabled right-click on cells.

--- script/main.mjs
+++ script/main.mjs
@@ -4,6 +4,7 @@ import { Interactions } from "./interactions.mjs";
 let aSrc = new CellPosition(0, 0);
 let aDest = new CellPosition(ROW - 1, COL - 1);
 let aGrid = [];
+
 (function initGrid() {
     for (let i = 0; i < ROW; ++i) {
         aGrid[i] = new Array(COL);
@@ -89,6 +90,9 @@ let aGrid = [];
         })
         .on("mousedown", function() {
             return Interactions.isDraggable(this);
+        })
+        .on("contextmenu", function() {
+            return false;
         });
 
     $("#start-icon").on("dragstart", function() {
