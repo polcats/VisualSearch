cabdadc0fc37a27d46d46b17a2fbcdae5aef5f4c
polcats <24451518+polcats@users.noreply.github.com>
2019-12-27 12:02:44 +0800

    Placed functions in a class. Added functions removed from index.

--- script/interface.mjs
+++ script/interface.mjs
@@ -1,31 +1,85 @@
-function toggleAddBlocks() {
-    $("input#add-block").toggleClass("active-button");
-}
+import { Algorithms } from "./algorithms.mjs";
+import { ROW, COL, CellPosition, Utility } from "./utilities.mjs";
 
-function clearPaths() {
-    $("td.path").removeClass("path");
-}
-
-function clearBlocks() {
-    $("td.blocked").removeClass("blocked");
-}
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function isDraggable(item) {
-    if (undefined === item.childNodes[0]) {
-        return false;
+class Interactions {
+    static toggleAddBlocks() {
+        $("input#add-block").toggleClass("active-button");
     }
 
-    return true;
+    static clearPaths() {
+        $("td.path").removeClass("path");
+    }
+
+    static clearBlocks() {
+        $("td.blocked").removeClass("blocked");
+    }
+
+    static allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    static isDraggable(item) {
+        if (undefined === item.childNodes[0]) {
+            return false;
+        }
+
+        return true;
+    }
+
+    currentDragged = "";
+    static drag(ev, id) {
+        this.currentDragged = id;
+        ev.dataTransfer.setData(id, ev.target.id);
+    }
+
+    static drop(ev) {
+        ev.preventDefault();
+        let data = ev.dataTransfer.getData(this.currentDragged);
+        let currentIcon = document.getElementById(data);
+
+        let result = [];
+        if (null === currentIcon) {
+            return result;
+        }
+
+        ev.target.appendChild(currentIcon);
+
+        let parentId = currentIcon.parentNode.id;
+        let cellIndices = parentId.split("-");
+
+        let newPosition = new CellPosition(cellIndices[0], cellIndices[1]);
+        if (currentIcon.id == "start-icon") {
+            result = [currentIcon.id, newPosition];
+        } else if (currentIcon.id == "goal-icon") {
+            result = [currentIcon.id, newPosition];
+        }
+
+        return result;
+    }
+
+    static findPath(aGrid, aSrc, aDest, heuristic) {
+        this.clearPaths();
+
+        let cells = Algorithms.aStarSearch(aGrid, aSrc, aDest, heuristic);
+        if (cells != false) {
+            Utility.tracePath(cells, aDest);
+        }
+    }
+
+    static clearGrid(aGrid) {
+        for (let i = 0; i < ROW; ++i) {
+            for (let j = 0; j < COL; ++j) {
+                aGrid[i][j] = 0;
+            }
+        }
+    }
+
+    static resetAll(aGrid) {
+        this.clearGrid(aGrid);
+        this.clearPaths();
+        this.clearBlocks();
+        $("input#add-block").removeClass("active-button");
+    }
 }
 
-let currentDragged = "";
-function drag(ev, id) {
-    currentDragged = id;
-    ev.dataTransfer.setData(id, ev.target.id);
-}
-
-export { currentDragged, isDraggable, toggleAddBlocks, clearPaths, clearBlocks, allowDrop, drag };
+export { Interactions };
