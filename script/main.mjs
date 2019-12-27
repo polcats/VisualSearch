32d1f9c4d43666c5af7c4937f5dce40cbce94990
polcats <24451518+polcats@users.noreply.github.com>
2019-12-27 20:24:05 +0800

    Adjusted table size based from cell count.

--- script/main.mjs
+++ script/main.mjs
@@ -14,6 +14,11 @@ let aGrid = [];
 })();
 
 (function generateCells() {
+    $("#cell-table").css({
+        width: COL * 50 + "px",
+        height: ROW * 50 + "px"
+    });
+
     for (let i = 0; i < ROW; ++i) {
         let row = $("<tr></tr>");
         for (let j = 0; j < COL; ++j) {
